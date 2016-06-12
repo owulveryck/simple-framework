1933452bc3d33745fea2c5e62136d96d155fe01e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-06-12 21:32:47 +0200

    For testing purpose

---
+++ htdocs/js/websocket.js
@@ -0,0 +1,17 @@
+var serversocket = new WebSocket("wss://lab.owulveryck.info:443/echo");
+
+serversocket.onopen = function() {
+    serversocket.send("Connection init");
+}
+
+// Write message on receive
+serversocket.onmessage = function(e) {
+    document.getElementById('output').innerHTML += "Received: " + e.data + "<br>";
+};
+
+function senddata() {
+    var data = document.getElementById('sendtext').value;
+    serversocket.send(data);
+    document.getElementById('output').innerHTML += "Sent: " + data + "<br>";
+}
+
